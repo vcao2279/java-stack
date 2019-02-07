d25038b7c55a1e6366f7ac302b4d677b6c8c99a4
Vu Cao <cdhvu@yahoo.com>
2019-02-07 15:00:32 -0800

    Implement push method for Stack

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main
 {
     public static void main (String[] args)
     {
+        Stack stack = new Stack();
+        stack.push("first");
+        stack.push("second");
+        stack.push("third");
 
+        System.out.println(stack);
     }
 }
--- src/com/vucao/Stack.java
+++ src/com/vucao/Stack.java
@@ -7,5 +7,21 @@ public class Stack
     private String[] stack = new String[1000];
     private int currentIndex = 0;
 
+    public void push(String s)
+    {
+        if (currentIndex == 1000)
+        {
+            System.out.println("Stack is full.");
+        } else
+        {
+            stack[currentIndex] = s;
+            currentIndex++;
+        }
+    }
 
+    @Override
+    public String toString()
+    {
+        return Arrays.toString(stack);
+    }
 }
